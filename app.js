8eca307f3b97c40a93a1cf1bcf4aa52b7b05d80f
Leo Garza <Leog4za@gmail.com>
2020-08-08 00:53:47 -0500

    addoing changes

--- app.js
+++ app.js
@@ -51,15 +51,15 @@ app.use(xss());
 
 
 
-app.use (function (req, res, next) {
-    if (req.secure) {
-            // request was via https, so do no special handling
-            next();
-    } else {
-            // request was via http, so redirect to https
-            res.redirect('https://' + req.headers.host + req.url);
-    }
-});
+// app.use (function (req, res, next) {
+//     if (req.secure) {
+//             // request was via https, so do no special handling
+//             next();
+//     } else {
+//             // request was via http, so redirect to https
+//             res.redirect('https://' + req.headers.host + req.url);
+//     }
+// });
 
 // Gets Time of request
 app.use((req, res, next) => {
