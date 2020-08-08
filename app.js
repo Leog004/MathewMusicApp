a8f94afdd9b064c41e15d3fa7c21890234bfaf6c
Leo Garza <Leog4za@gmail.com>
2020-08-08 00:45:23 -0500

    addoing changes

--- app.js
+++ app.js
@@ -51,6 +51,16 @@ app.use(xss());
 
 
 
+app.use (function (req, res, next) {
+    if (req.secure) {
+            // request was via https, so do no special handling
+            next();
+    } else {
+            // request was via http, so redirect to https
+            res.redirect('https://' + req.headers.host + req.url);
+    }
+});
+
 // Gets Time of request
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
